ae2b789bceb7c92ab6df97f2404f721fa5be0a74
Maccrey <m01071630214@gmail.com>
2025-03-12 17:49:13 +0900

    프로필 사진 이미지에 변경 아이콘 구현

--- lib/src/views/more/widgets/profile/profile_image_widget.dart
+++ lib/src/views/more/widgets/profile/profile_image_widget.dart
@@ -10,9 +10,37 @@ class ProfileImageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
-      radius: radius,
-      backgroundImage: AssetImage('lib/core/imgs/images/StudyDuck.png'),
+    return Stack(
+      children: [
+        GestureDetector(
+          onTap: () {
+            // Todo : 프로필 사진 변경 로직 추가
+            print('프로필 사진 변경');
+          },
+          child: CircleAvatar(
+            radius: radius,
+            backgroundImage: AssetImage('lib/core/imgs/images/StudyDuck.png'),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: GestureDetector(
+            onTap: () {
+              // 프로필 사진 변경 로직 추가
+            },
+            child: CircleAvatar(
+              radius: radius * 0.3,
+              backgroundColor: Colors.transparent,
+              child: Icon(
+                Icons.camera_alt,
+                size: radius * 0.3,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
